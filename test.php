ad06ab55e1dbb2ec56a481e8320e81a55125f0e5
himath-adithya <himathadithya@gmail.com>
2025-09-04 01:17:13 +0530

    feat: remove test.php file containing password hashing examples

--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $passhash1 = password_hash('Test123@', PASSWORD_DEFAULT);
-  $passhash2 = password_hash('Test456@', PASSWORD_DEFAULT);
-  echo "Password Hash 1: " . $passhash1 . "<br>";
-  echo "Password Hash 2: " . $passhash2 . "<br>";
-?>
