ae6f8ee6b1cc3e2dae2e6c6cadf63d2eabad3ae5
himath-adithya <himathadithya@gmail.com>
2025-09-03 16:08:08 +0530

    feat: implement user type toggle in login form and enhance styling

--- includes/headerHome.php
+++ includes/headerHome.php
@@ -31,8 +31,8 @@
         </div>
 
         <div class="button-wrapper">
-            <button onclick="window.location.href='client/login.php'">Log in</button>
-            <button onclick="window.location.href='signup.php'">Sign Up</button>
+            <button onclick="window.location.href='login.php'">Log in</button>
+            <button onclick="window.location.href='registration.php'">Sign Up</button>
         </div>
 
     </div>
