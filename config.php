89b51211190517cbf718d9442ff191672369825f
Pasindu Gihan <samaraweera.ghn@gmail.com>
2025-10-20 14:35:27 +0530

    DB Data to Match Online

--- config.php
+++ config.php
@@ -1,10 +1,19 @@
 <?php
 
 // Database credentials
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'servo');
+if ($_SERVER['SERVER_NAME'] === 'localhost') {
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'servo');
+} else {
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'webuser');
+    define('DB_PASS', 'Pass@Servo2025');
+    define('DB_NAME', 'servo');
+}
+
+
 
 define('BASE_URL', '.');
 
