1cf40ebf31a8facdf2f3fdf8d8ca81cb20d854f2
himath-adithya <himathadithya@gmail.com>
2025-10-30 10:18:44 +0530

    changed the login page to show the appropriate error status

--- app/models/ClientModel.php
+++ app/models/ClientModel.php
@@ -7,7 +7,7 @@ class ClientModel extends Database
 
     public function getByEmail($email)
     {
-        $stmt = $this->conn->prepare("SELECT * FROM Client WHERE Email = ? AND Status='active'");
+        $stmt = $this->conn->prepare("SELECT * FROM Client WHERE Email = ?");
         $stmt->bind_param("s", $email);
         $stmt->execute();
         $result = $stmt->get_result();
