1d546984b1d0da58fc2cfb934e2467656cf8559e
himath-adithya <himathadithya@gmail.com>
2025-11-15 21:04:49 +0530

    small tweak to landing page to display 'log out' button

--- app/views/landing.php
+++ app/views/landing.php
@@ -36,8 +36,8 @@
 			</div>
 			<div class="auth">
 					<?php if (isset($_SESSION['user_id']) && isset($_SESSION['role'])): ?>
+							<a href="<?= BASE_URL ?>/logout" class="login">Log Out</a>
 							<a href="<?= BASE_URL ?>/dashboard" class="cta">Dashboard</a>
-							<!-- <a href="<?= BASE_URL ?>/logout" class="cta">Log Out</a> -->
 					<?php else: ?>
 							<a href="<?= BASE_URL ?>/login" class="login">Log In</a>
 							<a href="<?= BASE_URL ?>/register" class="cta">Get Started</a>
